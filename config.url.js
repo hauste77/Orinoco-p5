e53be2c800825febcfe7302588622deb24e0a3d5
xav <xav.77176@hotmail.fr>
2021-05-01 20:53:00 +0200

    test heroku

--- config.url.js
+++ config.url.js
@@ -1,8 +1,8 @@
 // je crée une variable config qui me permet de changer les chemins URL utilisés plus facilement plutôt que de devoir les changer dans chaque fichier si besoin
 
 const config = {
-    apiBase: "http://localhost:3000",
-    frontBase: "https://orinoco77.herokuapp.com"
+    apiBase: "https://orinoco77.herokuapp.com/",
+    frontBase: "http://127.0.0.1:5500"
 }
 
 const url = `${config.apiBase}/api/teddies`;
