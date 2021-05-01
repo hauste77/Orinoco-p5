72135a880ad94b315022d24d26d2d54621964671
xav <xav.77176@hotmail.fr>
2021-05-01 21:11:13 +0200

    réglage url

--- config.url.js
+++ config.url.js
@@ -1,7 +1,7 @@
 // je crée une variable config qui me permet de changer les chemins URL utilisés plus facilement plutôt que de devoir les changer dans chaque fichier si besoin
 
 const config = {
-    apiBase: "https://orinoco77.herokuapp.com/",
+    apiBase: "https://orinoco77.herokuapp.com",
     frontBase: "http://127.0.0.1:5500"
 }
 
